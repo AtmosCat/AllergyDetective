f49914ee80fb12a34f17476dafec110050df9f9f
hojeong_jeong <dnflro22@gmail.com>
2024-10-09 22:10:49 +0900

    같은 페이지에서 1, 2, 3, 4번 각각에 대한 크롤링 구현 완료. megacoffeeFoodCrawler 구현 작업 필요

--- app/src/main/java/com/allergyguardian/allergyguardian/presentation/franchise/franchise_crawler/FranchiseCrawlerFragment.kt
+++ app/src/main/java/com/allergyguardian/allergyguardian/presentation/franchise/franchise_crawler/FranchiseCrawlerFragment.kt
@@ -26,7 +26,7 @@ class FranchiseCrawlerFragment : Fragment() {
 
     private var starbucksBevList: MutableList<Menu> = mutableListOf()
     private var twosomeList: MutableList<Menu> = mutableListOf()
-    private var megacoffeeList: MutableList<Menu> = mutableListOf()
+    private var megacoffeeBevList: MutableList<Menu> = mutableListOf()
 
     private var imgurlList = mutableListOf<String>()
     private var nameList = mutableListOf<String>()
@@ -58,7 +58,7 @@ class FranchiseCrawlerFragment : Fragment() {
 
 //        starbucksBevCrawler(franchiseViewModel.starbucksBeverageUrls)
 //        twosomeCrawler(franchiseViewModel.twosomeUrls)
-        megacoffeeCrawler(franchiseViewModel.megacoffeeUrls)
+        megacoffeeBevCrawler(franchiseViewModel.megacoffeeUrls, 4)
     }
 
     override fun onDestroyView() {
@@ -92,8 +92,8 @@ class FranchiseCrawlerFragment : Fragment() {
         )
     }
 
-    private fun megacoffeeCrawler(urls: List<String>) {
-        val iterator = urls.iterator()
+    private fun megacoffeeBevCrawler(urls: List<String>, totalPages: Int) {
+        val iterator = (1..totalPages).iterator()
         loadNextUrl2(
             iterator,
             "카페",
@@ -101,7 +101,9 @@ class FranchiseCrawlerFragment : Fragment() {
             "Array.from(document.querySelectorAll('.cont_gallery_list_img img')).map(img => img.src)", // 선택자는 이상없음
             "Array.from(document.querySelectorAll('.inner_modal .cont_text_box .cont_text_title b')).map(b => b.innerText)", // 선택자 작동함
             "Array.from(document.querySelectorAll('.cont_text.cont_text_info')).map(el => el.innerText)",
-            megacoffeeList
+            megacoffeeBevList,
+            totalPages,
+            "https://www.mega-mgccoffee.com/menu/?menu_category1=1&menu_category2=1"
         )
     }
 
@@ -134,47 +136,56 @@ class FranchiseCrawlerFragment : Fragment() {
         }
     }
 
-    fun loadNextUrl2(iterator: Iterator<String>, type: String, brand: String, imgSelector: String, nameSelector: String,
-                    allergySelector: String, menuList: MutableList<Menu>) {
+    fun loadNextUrl2(iterator: Iterator<Int>, type: String, brand: String, imgSelector: String, nameSelector: String,
+                    allergySelector: String, menuList: MutableList<Menu>, totalPages: Int, url: String) {
         if (iterator.hasNext()) {
-            val url = iterator.next()
-            binding.webviewMenu.loadUrl(url)
+            val pageNumber  = iterator.next()
             binding.webviewMenu.webViewClient = object : WebViewClient() {
                 override fun onPageFinished(view: WebView, url: String) {
                     super.onPageFinished(view, url)
-                        binding.webviewMenu.evaluateJavascript(imgSelector) { value ->
-                            val listType = object : TypeToken<List<String>>() {}.type
-                            val imageUrls: MutableList<String> = Gson().fromJson(value, listType)
-                            imgurlList = imageUrls
-                        }
-                        binding.webviewMenu.evaluateJavascript(nameSelector) { value ->
-                            val listType = object : TypeToken<List<String>>() {}.type
-                            val names: MutableList<String> = Gson().fromJson(value, listType)
-                            nameList = names
-                        }
-                        binding.webviewMenu.evaluateJavascript(allergySelector) { value ->
-                            val listType = object : TypeToken<List<String>>() {}.type
-                            var allergies: MutableList<String> = Gson().fromJson(value, listType)
-                            allergies = allergies.filter { it.contains("알레르기") }.toMutableList()
-                            val allergiesSize = allergies.size
-                            for (i in 0..allergiesSize-1) {
-                                allergies[i] = allergies[i].replace("\n", "")
-                                allergies[i] = allergies[i].replace("\t", "")
+                    binding.webviewMenu.evaluateJavascript("document.querySelector('.board_page_link[data-page=\"$pageNumber\"]').click();") { result ->
+                        // 페이지 로드 후 데이터 추출
+                        // 약간의 지연 후에 데이터 추출
+                        binding.webviewMenu.postDelayed({
+                            binding.webviewMenu.evaluateJavascript(imgSelector) { value ->
+                                val listType = object : TypeToken<List<String>>() {}.type
+                                val imageUrls: MutableList<String> =
+                                    Gson().fromJson(value, listType)
+                                imgurlList = imageUrls
                             }
-                            allergyList = allergies
-                        }
-                        for (i in 0..nameList.size-1) {
-                            menuList.add(
-                                Menu(
-                                    imgurl = imgurlList[i],
-                                    name = nameList[i],
-                                    allergy = allergyList[i]
+                            binding.webviewMenu.evaluateJavascript(nameSelector) { value ->
+                                val listType = object : TypeToken<List<String>>() {}.type
+                                val names: MutableList<String> = Gson().fromJson(value, listType)
+                                nameList = names
+                            }
+                            binding.webviewMenu.evaluateJavascript(allergySelector) { value ->
+                                val listType = object : TypeToken<List<String>>() {}.type
+                                var allergies: MutableList<String> =
+                                    Gson().fromJson(value, listType)
+                                allergies = allergies.filter { it.contains("알레르기") }.toMutableList()
+                                val allergiesSize = allergies.size
+                                for (i in 0..allergiesSize - 1) {
+                                    allergies[i] = allergies[i].replace("\n", "")
+                                    allergies[i] = allergies[i].replace("\t", "")
+                                }
+                                allergyList = allergies
+                            }
+                            for (i in 0..nameList.size - 1) {
+                                menuList.add(
+                                    Menu(
+                                        imgurl = imgurlList[i],
+                                        name = nameList[i],
+                                        allergy = allergyList[i]
+                                    )
                                 )
-                            )
-                        }
-                    loadNextUrl2(iterator, type, brand, imgSelector, nameSelector, allergySelector, menuList)
+                            }
+                            loadNextUrl2(iterator, type, brand, imgSelector, nameSelector,
+                                allergySelector, menuList, totalPages, url)
+                        }, 1000)
+                    }
                 }
             }
+            binding.webviewMenu.loadUrl(url)
         }
     }
 
